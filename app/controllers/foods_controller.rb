07ec956980d68f0c4b67797bfc5375b450d0b53a
Precious Akams <preciousakams@yahoo.com>
2022-09-13 13:46:22 +0100

    added authentication

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,11 @@
 class FoodsController < ApplicationController
-  before_action :set_food, only: %i[show edit update destroy]
+  before_action :authenticate_user!
+  
 
   # GET /foods or /foods.json
   def index
     @foods = Food.all
   end
-
-  # GET /foods/1 or /foods/1.json
-  def show; end
 
   # GET /foods/new
   def new
@@ -29,19 +27,6 @@ class FoodsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /foods/1 or /foods/1.json
-  def update
-    respond_to do |format|
-      if @food.update(food_params)
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully updated.' }
-        format.json { render :show, status: :ok, location: @food }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /foods/1 or /foods/1.json
   def destroy
     @food.destroy
@@ -53,14 +38,8 @@ class FoodsController < ApplicationController
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_food
-    @food = Food.find(params[:id])
-  end
-
   # Only allow a list of trusted parameters through.
   def food_params
-    params.require(:food).permit(:name, :measurment_unit, :price)
+    params.require(:food).permit!
   end
 end
