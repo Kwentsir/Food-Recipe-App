252c7795a86b8a297f3749d526ce7be5139b2750
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-09-15 08:40:24 +0200

    generates database inventories, inventory_food tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_14_125134) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_062555) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_125134) do
     t.string "name"
     t.string "measurment_unit"
     t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inventories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inventory_foods", force: :cascade do |t|
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
